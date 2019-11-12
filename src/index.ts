61b9e591c1f65fdaac99b5c16f45fc240506b2cc
Stefan Kerschbaumer <kerschbaumer@lean-coders.at>
2019-11-12 17:02:34 +0100

    added structure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
+// need to be implemented first!!
 import './core/polyfills';
-import {EventListenerHook} from "./core/eventListenerHook";
 
 import {MainController} from "./controller/mainController";
 import {LoBroModule} from "./core/loBroModule";
 
-let eventListenerHook: EventListenerHook = new EventListenerHook();
-let controller: MainController = new MainController();
-window['controller'] = controller;
+
+// let controller: MainController = new MainController();
+// window['controller'] = controller;
 const module: LoBroModule = new LoBroModule({
-    controller: [controller],
+    controller: [MainController],
 });
-eventListenerHook.setModule(module);
-// bootstrap<MainController>(MainController);
-// const [mainControllerInstance, release1] = bootstrap<MainController>(MainController);
+
+module.bootstrap();
